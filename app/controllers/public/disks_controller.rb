e8a75c6ef604aa7b532cb2c935a5b3917fc6e340
KaichiIwahara <157882052+kai159874@users.noreply.github.com>
2024-07-03 03:01:29 +0000

    [add]disk controller

--- app/controllers/public/disks_controller.rb
+++ app/controllers/public/disks_controller.rb
@@ -1,13 +1,38 @@
 class Public::DisksController < ApplicationController
+  before_action :ensure_disk, only: [:show, :edit, :update, :destroy]
+  
   def new
+    @disk = Disk.new
+  end
+  
+  def create
+    @disk = Disk.new(disk_params)
   end
 
   def show
   end
 
   def index
+    @disks = Disk.all
   end
 
   def edit
   end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  private
+  
+  def ensure_disk
+    @disk = Disk.find(params[:id])
+  end
+  
+  def disk_params
+    params.require(:disk).permit(:title, :introduction, :jacket_image, :file)
+  end
+  
 end
