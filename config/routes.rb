31216eeca5a208b99cff82563f6c9291aa1e18be
KaichiIwahara <157882052+kai159874@users.noreply.github.com>
2024-07-04 06:49:33 +0000

    [add]header

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,22 @@ Rails.application.routes.draw do
 
   root to: "public/homes#top"
 
-  devise_for :admin,skip: [:passwords], controllers: {
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  devise_for :users, skip: [:registrations, :passwords] , controllers: {
+  devise_for :users, skip: [:passwords] , controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
 
-  namespace :public do
-    get 'disks/new'
-    get 'disks/show'
-    get 'disks/index'
-    get 'disks/edit'
+  scope module: :public do
+    get   'mypage'                  => 'users#mypage', as: "mypage"
+    get   'mypage/edit'             => 'users#edit', as: "mypage_edit"
+    patch 'mypage'                  => 'users#update'
+    get   'unsubscribe'             => 'users#unsubscribe', as: "unsubscribe"
+    patch 'withdrow'                => 'users#withdrow', as: "withdrow"
+    resources :disks
+    resources :users, only: [:show]
   end
 
 
