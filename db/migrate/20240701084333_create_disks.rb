b2df1c5c1e78e427d41457a778b351f846a10220
KaichiIwahara <157882052+kai159874@users.noreply.github.com>
2024-07-07 03:30:02 +0000

    [add]disk new

--- db/migrate/20240701084333_create_disks.rb
+++ db/migrate/20240701084333_create_disks.rb
@@ -1,13 +1,13 @@
 class CreateDisks < ActiveRecord::Migration[6.1]
   def change
     create_table :disks do |t|
-      
+
       t.references  :user,          null: false, foreign_key: true
       t.string      :title,         null: false
       t.string      :introduction
       t.string      :file,          null: false
-      t.boolean     :on_public,     null: false
-      
+      t.boolean     :on_public,     null: false, default: true
+
 
       t.timestamps
     end
